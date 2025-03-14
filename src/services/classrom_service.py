d2d4b8c169c989c4ab5771da82044e72b2234026
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-14 12:10:14 -0300

    Add ClassRoomService for handling classroom-related operations

---
+++ src/services/classrom_service.py
@@ -0,0 +1,17 @@
+from chains.classroom_chain import ClassRoomChain
+from schemas.classroom_schema import ClassRoomInput
+from config.env_load import EnvLoad 
+
+
+class ClassRoomService:
+
+    def __init__(self, broker):
+        self.rabbitmq = broker
+        env = EnvLoad()
+        self.config = env.get_variables()
+
+
+    async def execute(self, weeklyStudyPlanDetail: ClassRoomInput):
+        class_room_chain = ClassRoomChain(self.config.open_api_key)
+        result = await class_room_chain.execute(weeklyStudyPlanDetail, self.config.open_api_key)
+        return result
