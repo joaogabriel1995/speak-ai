6a74c76f91ccb37ea548ef1591bfc4e949d3f890
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-14 12:06:05 -0300

    Add main asynchronous RabbitMQ consumer setup

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import asyncio
+import json
+import aio_pika
+from config.rabbitmq import RabbitMQ
+from consumers.learning_journey_consumer import learning_journey_consumer
+from consumers.learning_detail_consumer import learning_detail_consumer
+from consumers.classroom_consumer import classroom_consumer
+
+rabbitmq = RabbitMQ()
+
+
+async def main():
+    """
+    Função principal que configura a conexão com RabbitMQ,
+    assina as filas e aguarda indefinidamente.
+    """
+    try:
+        RabbitMQ.set_url("amqp://guest:guest@localhost:5672")
+        await rabbitmq.connection()
+        await rabbitmq.create_channel()
+
+        await rabbitmq.subscribe("agent-plan-study", learning_journey_consumer)
+        await rabbitmq.subscribe("weekly_plan_queue", learning_detail_consumer)
+        await rabbitmq.subscribe("classroom_queue", classroom_consumer)
+        print("entrei")
+        # Mantém o loop rodando
+        await asyncio.Future()
+
+    except Exception as e:
+        print(f"Erro ao processar: {e}")
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
