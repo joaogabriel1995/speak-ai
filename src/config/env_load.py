905d5a7eca1790f22853f982fefd953ac8466969
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-14 12:08:36 -0300

    Add EnvLoad class for loading environment variables from a .env file

---
+++ src/config/env_load.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+
+
+class EnvLoad:
+    def __init__(self, env_file: str = "../.env"):
+        """
+        Inicializa a classe e carrega as variáveis do arquivo .env em um dicionário.
+        """
+        load_dotenv(env_file)
+        self.variables = {"open_api_key": os.getenv("OPENAI_API_KEY")}
+
+    def get_variables(self):
+        """
+        Retorna todas as variáveis carregadas.
+        """
+        return self.variables
+
+
+# Exemplo de uso
+if __name__ == "__main__":
+    env = EnvLoad()
+    print(env.get_variables())
