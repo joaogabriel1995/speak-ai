611d56b5f2decfe79543dfc3cebcdb8be7f32ed3
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-17 23:55:27 -0300

    Refactor ClassRoomService to improve environment variable access and streamline execution method

--- src/services/classrom_service.py
+++ src/services/classrom_service.py
@@ -1,6 +1,6 @@
 from chains.classroom_chain import ClassRoomChain
 from schemas.classroom_schema import ClassRoomInput
-from config.env_load import EnvLoad 
+from config.env_load import EnvLoad
 
 
 class ClassRoomService:
@@ -10,8 +10,8 @@ class ClassRoomService:
         env = EnvLoad()
         self.config = env.get_variables()
 
-
     async def execute(self, weeklyStudyPlanDetail: ClassRoomInput):
-        class_room_chain = ClassRoomChain(self.config.open_api_key)
-        result = await class_room_chain.execute(weeklyStudyPlanDetail, self.config.open_api_key)
+        class_room_chain = ClassRoomChain(self.config.get("open_api_key"))
+        result = await class_room_chain.execute(weeklyStudyPlanDetail)
+
         return result
